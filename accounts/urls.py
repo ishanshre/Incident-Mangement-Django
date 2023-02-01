dc7251426d9cba07ab67cd95026479df30c849b1
Ishan Shrestha <ishan.shrestha356@gmail.com>
2023-02-01 08:33:16 +0545

    feat: create, view and update profile feature     - create profile form     - add a view logic for viewing and updating profile     - add profile urls     - create profile template     - remove null and blank attribute in image field of profile     - add babel package for phonenumbers prefix select

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', views.UserRegisterView.as_view(), name='register'),
+    path('profile/', views.ProfileDetailAndUpdateView.as_view(), name="profile"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import CreateView
+from django.views import View
 
 
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.urls import reverse_lazy
 
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib import messages
 
-from accounts.forms import CustomUserCreationForm, UserLoginForm
+from accounts.forms import CustomUserCreationForm, UserLoginForm, ProfileForm, CustomUserChangeForm
+from accounts.models import Profile
 # Create your views here.
 
 
@@ -43,3 +46,35 @@ class UserLoginView(SuccessMessageMixin, LoginView):
         if request.user.is_authenticated:
             return redirect('core:index')
         return super(UserLoginView,self).dispatch(request, *args, **kwargs)
+
+
+
+class ProfileDetailAndUpdateView(LoginRequiredMixin, View):
+    template_name = "accounts/profile.html"
+    def get(self, request, *args, **kwargs):
+        profile = get_object_or_404(Profile, user=request.user)
+        profile_form = ProfileForm(instance=profile)
+        user_form = CustomUserChangeForm(instance=request.user)
+        context = {
+            "profile":profile,
+            "profile_form":profile_form,
+            "user_form":user_form,
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        print(request.FILES)
+        profile = get_object_or_404(Profile, user=request.user)
+        profile_form = ProfileForm(request.POST, request.FILES, instance=profile)
+        user_form = CustomUserChangeForm(request.POST, instance=request.user)
+        if profile_form.is_valid() and user_form.is_valid():
+            profile_form.save()
+            user_form.save()
+            messages.success(request, "Profile Updated Successfully")
+            return redirect("accounts:profile")
+        context = {
+            "profile":profile,
+            "profile_form":profile_form,
+            "user_form":user_form,
+        }
+        return render(request, self.template_name, context)
